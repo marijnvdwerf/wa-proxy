4525cb5d4b3243509de70e0f699eb3750ef6674e
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2013-10-10 14:01:49 +0200

    Post new messages to server

--- app/www/scripts/models/Conversation.js
+++ app/www/scripts/models/Conversation.js
@@ -10,6 +10,19 @@ define([
         },
 
         idAttribute:"_id"
+
+        sendMessage: function (message) {
+            var self = this;
+
+            $.ajax('http://marijnvdwerf-server.jit.su/conversations/' + this.get('identifier'), {
+                method: 'POST',
+                data: {
+                    message: message
+                }
+            }).success(function () {
+                    alert('sent');
+                });
+        }
     });
 
 });
--- app/www/scripts/views/Conversation.js
+++ app/www/scripts/views/Conversation.js
@@ -4,6 +4,10 @@ define([
 ], function ($, Backbone) {
 
     return Backbone.View.extend({
+        events: {
+            'submit form': 'postMessage'
+        },
+
         render: function () {
             this.template = _.template($('script.conversation').html(), this.model.toJSON());
             console.log(this.$el.find('[data-role="content"]'));
@@ -13,6 +17,12 @@ define([
             this.$el.find('[data-role="header"] h1').text(this.model.get('name'));
 
             return this;
+        },
+
+        postMessage: function() {
+            var message = this.$el.find('input[name=message]').val();
+            this.model.sendMessage(message);
+            return false;
         }
     });
 
