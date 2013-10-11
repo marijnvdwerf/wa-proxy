4620ad6be2f3d46e638a0d6730f1c396ba52608d
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2013-10-11 10:04:31 +0200

    Always keep conversationview styled

--- app/www/scripts/views/Conversation.js
+++ app/www/scripts/views/Conversation.js
@@ -10,17 +10,22 @@ define([
 
         render: function () {
             console.log(this.model.get('identifier'));
-            this.template = _.template($('script.conversation').html(), {conversation: this.model.toJSON(), messages:this.model.get("messages").toJSON()});
+            this.template = _.template($('script.conversation').html(), {conversation: this.model.toJSON(), messages: this.model.get("messages").toJSON()});
             console.log(this.$el.find('[data-role="content"]'));
             this.$el.find('[data-role="content"]')
                 .html(this.template);
+
+            if (this.$el.data('mobile-page') !== undefined) {
+                // After the first appearance, we need to notify the page that it needs to be styled
+                this.$el.trigger('create');
+            }
 
             this.$el.find('[data-role="header"] h1').text(this.model.get('name'));
 
             return this;
         },
 
-        postMessage: function() {
+        postMessage: function () {
             var message = this.$el.find('input[name=message]').val();
             this.model.sendMessage(message);
             return false;
